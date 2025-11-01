b32ebcd78f954e89c3db75d94f2a2644fd5db1e5
Keril-valle <kvalle1306@gmail.com>
2025-11-01 11:00:56 -0600

    agregar las clases de servicos

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ServiciosModule } from './servicios/servicios.module';
 
 @Module({
-  imports: [],
+  imports: [ServiciosModule],
   controllers: [AppController],
   providers: [AppService],
 })
